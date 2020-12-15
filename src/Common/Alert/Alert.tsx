15c0836c36571cc1f34ffcddd406d3b38aea90ec
Tyler Darsey <bulldogboy912000@yahoo.com>
2020-12-15 17:01:46 -0600

    Little docing

--- src/Common/Alert/Alert.tsx
+++ src/Common/Alert/Alert.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Alert.scss";
 
-export type AlertType = "warning" | "danger" | "success";
+export type AlertType = "danger" //"warning" | "success" | "info"
 
 interface AlertProps {
     type: AlertType,
     children: any
 }
 
+/**
+ * Simple alert
+ * 
+ * @param AlertType - Symantic type of the alert.
+ */
 export const Alert = ({ type, children }: AlertProps) => {
     return <div className={`alert alert-${type}`}>
         {children}
