7e64b730576f6949ec879b7528c2a952db22f98e
Tyler Darsey <bulldogboy912000@yahoo.com>
2020-12-15 13:39:42 -0600

    Fix casing issue

--- src/Data/Api.test.ts
+++ src/Data/Api.test.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { _getData, data_url, getItemGroups } from "./Api";
+import { _getData, data_url, getItemGroups } from "./api";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
