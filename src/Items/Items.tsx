92207c2ff9c2b3f95135c63138da4ea56286bb15
Tyler Darsey <bulldogboy912000@yahoo.com>
2020-12-15 17:43:24 -0600

    More styling

--- src/Items/Items.tsx
+++ src/Items/Items.tsx
@@ -2,8 +2,6 @@ import { ItemType, ItemGroupType } from "../data/types";
 
 import "./Items.scss";
 
-
-
 /**
  * Displays a list of item groups.
  */
@@ -28,7 +26,7 @@ export const ItemGroup = ({ itemGroup }: ItemGroupProps) => {
     const getNumericNameFromItem = (item: ItemType) => parseInt(item.name.replace(/\D/g,''), 10);
     const sortItems = (itemA: ItemType, itemB: ItemType) => getNumericNameFromItem(itemA) - getNumericNameFromItem(itemB)
     return (<div className="itemGroup">
-        <h2>Group {itemGroup.id} </h2>
+        <h2>List ID: {itemGroup.id} </h2>
         {  itemGroup.items.sort(sortItems).map((item) => <Item key={item.id} item={item} />)}
     </div>)
 }
@@ -39,4 +37,7 @@ export const ItemGroup = ({ itemGroup }: ItemGroupProps) => {
 interface ItemProps {
     item: ItemType
 }
-export const Item = ({ item }: ItemProps) => <div className="item">{item.name}</div>
+export const Item = ({ item }: ItemProps) => <div className="item">
+    <div className="item--id">(ID: {item.id})</div>
+    <div className="item--name">{item.name}</div>
+</div>
