818c6c6311d0fa9a0adce1a5429c65231d6dc418
Tyler Darsey <bulldogboy912000@yahoo.com>
2020-12-15 13:15:37 -0600

    Add missing property to test

--- src/Items/ItemListWrapper.test.tsx
+++ src/Items/ItemListWrapper.test.tsx
@@ -8,7 +8,7 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe("ItemListWrapper", () => {
     test('Renders', () => {
-        const wrapper = shallow(<ItemListWrapper />);
+        const wrapper = shallow(<ItemListWrapper mock={true}/>);
         expect(wrapper).toBeTruthy();
     })
 })
